380a3a8d3c3df564c019e6f9e7f6c03978f9f10f
Ravin Singh D <ravinsinghd@outlook.com>
2025-04-12 21:26:24 +0530

    Added sidenav open and close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'open-books-web';
+  isSidenavOpen = signal(false);
+
+  toggleSidenav() {
+    this.isSidenavOpen.update((prev) => !prev);
+  }
 }
