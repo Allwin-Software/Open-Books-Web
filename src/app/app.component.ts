cc8020a677dfad0236ca62a69951c1ac07e9e7a8
Ravin Singh D <ravinsinghd@outlook.com>
2025-04-13 23:40:03 +0530

    Added side nav items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { SIDE_NAV_ITEMS } from './constants/side-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'open-books-web';
   isSidenavOpen = signal(false);
+  sideNavItems = computed(() => SIDE_NAV_ITEMS);
 
   toggleSidenav() {
     this.isSidenavOpen.update((prev) => !prev);
---
+++ src/app/constants/side-nav.ts
@@ -0,0 +1,17 @@
+export const SIDE_NAV_ITEMS = [
+  {
+    title: 'Expenses',
+  },
+  {
+    title: 'Cost Centers',
+  },
+  {
+    title: 'Reports',
+  },
+  {
+    title: 'Settings',
+  },
+  {
+    title: 'Logout',
+  },
+];
