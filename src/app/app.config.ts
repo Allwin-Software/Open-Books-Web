f28d43c49d874de5b12a39b7617eb0ff02b2d13f
Ravin Singh D <ravinsinghd@outlook.com>
2025-04-19 21:08:41 +0530

    Added auth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
 
@@ -13,5 +14,12 @@ export const appConfig: ApplicationConfig = {
     provideNativeDateAdapter(),
     provideHttpClient(),
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    provideAuth0({
+      domain: 'dev-ejnpj35lmuujkmp3.us.auth0.com',
+      clientId: 'oGWDoddhbS2F5ZocQrtlMaDzD0WZjPwl',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
   {
     path: '',
+    canActivate: [AuthGuard],
     loadComponent: () => import('./expenses/expenses.component').then((m) => m.ExpensesComponent),
   },
   {
